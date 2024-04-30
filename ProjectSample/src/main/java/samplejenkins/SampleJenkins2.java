ec279d0c54aac234534f4b8cd4253f3696a1c25a
ANU ARJUN <ANU ARJUN@DESKTOP-211NE0A>
2024-04-30 11:53:03 +0530

    Created and uploaded SampleJenkins2 file

---
+++ ProjectSample/src/main/java/samplejenkins/SampleJenkins2.java
@@ -0,0 +1,10 @@
+package samplejenkins;
+
+public class SampleJenkins2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Anu");
+		System.out.println("Anu");
+	}
+}
