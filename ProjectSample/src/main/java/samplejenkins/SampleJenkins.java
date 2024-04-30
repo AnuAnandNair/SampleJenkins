813b08177a8b8a9509191c89cb289176f58760a9
ANU ARJUN <ANU ARJUN@DESKTOP-211NE0A>
2024-04-30 11:25:59 +0530

    Updated

---
+++ ProjectSample/src/main/java/samplejenkins/SampleJenkins.java
@@ -0,0 +1,14 @@
+package samplejenkins;
+
+public class SampleJenkins {
+	public void display() {
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleJenkins sj=new SampleJenkins();
+		sj.display();
+	}
+
+}
